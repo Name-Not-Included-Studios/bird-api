dd8fa7e0bbd1d9574acd6f8124afad9dc56d932e
Brice Duke <bricewduke@gmail.com>
2022-12-03 17:21:54 -0600

    chore(env): add example file

--- src/index.ts
+++ src/index.ts
@@ -1,36 +1,41 @@
-import "reflect-metadata";
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import { buildSchema } from "type-graphql";
-import { UserResolver } from "./resolvers/UserResolver";
-import neo4j from "neo4j-driver";
-import { config } from "dotenv";
+import 'reflect-metadata';
+
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { config } from 'dotenv';
+import neo4j from 'neo4j-driver';
+import { buildSchema } from 'type-graphql';
+
+import { UserResolver } from './resolvers/UserResolver';
 
 config();
 
 export const neo4jDriver = neo4j.driver(
-  process.env.NEO4J_URI || "",
-  neo4j.auth.basic(process.env.NEO4J_USERNAME || "", process.env.NEO4J_PASSWORD || "")
+	process.env.NEO4J_URI || "",
+	neo4j.auth.basic(
+		process.env.NEO4J_USERNAME || "",
+		process.env.NEO4J_PASSWORD || ""
+	)
 );
 
 const main = async () => {
-  const schema = await buildSchema({
-    resolvers: [UserResolver],
-  });
+	const schema = await buildSchema({
+		resolvers: [UserResolver],
+	});
 
-  const server = new ApolloServer({
-    schema,
-  });
+	const server = new ApolloServer({
+		schema,
+	});
 
-  const { url } = await startStandaloneServer(server, {
-    listen: { port: 4000 },
-  });
+	const { url } = await startStandaloneServer(server, {
+		listen: { port: 4000 },
+	});
 
-  console.log(`🚀  Server ready at: ${url}`);
+	console.log(`🚀  Server ready at: ${url}`);
 };
 
 main();
 
 process.on("exit", async () => {
-  await neo4jDriver.close();
+	await neo4jDriver.close();
 });
