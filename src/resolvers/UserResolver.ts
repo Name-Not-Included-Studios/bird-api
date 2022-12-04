a71da4d1ddb690f2018bc9584f7a71c9f0211161
ThatColdToast <72617136+ThatColdToast@users.noreply.github.com>
2022-12-03 18:14:26 -0600

    chore(userresolver): removed unneccecarry comments

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,105 +1,42 @@
-import { Args, Query, Resolver } from 'type-graphql';
+import { Args, Query, Resolver } from "type-graphql";
 
-import { neo4jDriver } from '../';
-import { User, UserNode } from '../schemas/User/User';
-import { UserArgs } from '../schemas/User/UserArgs';
+import { neo4jDriver } from "../";
+import { User, UserNode } from "../schemas/User/User";
+import { UserArgs } from "../schemas/User/UserArgs";
 
 // import { Record } from "neo4j-driver";
 
 @Resolver(User)
 export class UserResolver {
-	@Query(() => [User])
-	async getUsers(@Args() _args: UserArgs /**/): Promise<User[]> {
-		const session = neo4jDriver.session();
+  @Query(() => [User])
+  async getUsers(@Args() _args: UserArgs): Promise<User[]> {
+    const session = neo4jDriver.session();
 
-		let response: User[] = [];
+    let response: User[] = [];
 
-		/*
-      There are 3 ways of doing this:
-        Subscribe Events
-        The Promise Way
-        In a Transaction
-      
-      There is also an alternative for each of these called a Reactive Session
-      which I couldn't get working
-      
-      The first 2 will always run async and
-      will NOT wait for the records to continue execution
+    // run cypher in a transaction
+    const txc = session.beginTransaction();
 
-      The third does wait for the records to return therefore it 
-      can add to the res (Response) array before getUsers() returns
-      
-      This is great right? Yes, but it takes 500ms+
-      when I can run the same query in the neo4j browser
-      and have req and res in less than 2 ms... Woah, that's over the internet
-      that's like REALLY good
+    try {
+      const result = await txc.run<{ a: UserNode }>(`match (a:User) return a`);
 
-      Also, even though I am returning a User[] from this function (as specified in docs),
-      the Apollo web interface is giving me this:
+      await txc.commit();
+      console.log("committed");
 
-        {
-          "data": {},
-          "error": {
-            "message": "JSON.parse: unexpected character at line 1 column 1 of the JSON data"
-          }
-        }
-    */
+      result.records.map((r) => {
+        response.push(r.get("a").properties);
+      });
+    } catch (error) {
+      console.log(error);
+      await txc.rollback();
+      console.log("rolled back");
+    } finally {
+      await session.close();
+    }
 
-		// Subscribe Events
-		// session.run(`match (n:User {id: ${args.id}}) return n`, {}).subscribe({
-		//   onKeys: (/*keys: string[]*/) => {
-		//     // console.log(keys);
-		//   },
-		//   onNext: (record: Record) => {
-		//     const n = record.get("n");
-		//     response.push(n);
-		//     console.log(n);
-		//   },
-		//   onCompleted: () => {
-		//     session.close(); // returns a Promise
-		//   },
-		//   onError: (error) => {
-		//     console.log(error);
-		//   },
-		// });
-
-		// The Promise Way
-		// session
-		//   .run(`match (n:User) return n`, {})
-		//   .then((result) => {
-		//     result.records.forEach((record) => {
-		//       console.log(record.get("n"));
-		//       response.push(record.get("n"));
-		//     });
-		//   })
-		//   .catch((error) => {
-		//     console.log(error);
-		//   })
-		//   .then(() => session.close());
-
-		// run statement in a transaction
-		const txc = session.beginTransaction();
-
-		try {
-			const result = await txc.run<{ a: UserNode }>(`match (a:User) return a`);
-
-			await txc.commit();
-			console.log("committed");
-
-			result.records.map((r) => {
-				response.push(r.get("a").properties);
-			});
-		} catch (error) {
-			console.log(error);
-			await txc.rollback();
-			console.log("rolled back");
-		} finally {
-			await session.close();
-		}
-
-		// Just print and return the response
-		// console.log("RETURN:");
-		console.log(response);
-		return response;
-	}
+    // Just print and return the response
+    // console.log("RETURN:");
+    console.log(response);
+    return response;
+  }
 }
